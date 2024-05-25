fe6657ebe9487bb91b84c7f8230e6bfaa1838bae
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 21:05:12 +0530

    Update question10.c

--- question10.c
+++ question10.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#define PI 3.14159265358979323846 
+//kartikSingh comment 
+//2303511530009
 
+int main() {
+    float radius, volume;
+
+    printf("Input the radius of the sphere: ");
+    scanf("%f", &radius);
+
+    volume = (4.0 / 3.0) * PI * radius * radius * radius;
+
+    printf("The volume of the sphere is %f.\n", volume);
+
+    return 0;
+}
