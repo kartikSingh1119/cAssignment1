de9fd535e05f431d5ae19b55e2dad406a519708d
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:55:45 +0530

    Update question2.c

--- question2.c
+++ question2.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main() {
+//2.Print "Hello" in the first line and "KGI !!" in the second line.#include <stdio.h>
+//kartikSingh
+  //2303511530009
+    printf("Hello\nKGI !!\n");
+    return 0;
+}
 
