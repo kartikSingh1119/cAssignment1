1226effe18c7aa26f2ec567372e684cc994d3db0
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:58:04 +0530

    Update question4.c

--- question4.c
+++ question4.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+/* 
+4. Write a program to print \n and \r on the screen.
+*/
+// kartikSingh
+//2303511530009
+int main() {
+    printf("\\n\n");
+    printf("\\r\n");
+    return 0;
+}
